f01d1403aff44cb09e93665df3dbb7129d4c0e3f
gabalicious <gabalicious113@gmail.com>
2015-02-28 16:37:18 -0500

    Modify Seed File

--- db/seeds.rb
+++ db/seeds.rb
@@ -10,13 +10,41 @@ topic3 = Topic.create(category: "Phase 2 / Week 1" , title: "RESTful Routes")
 
 
 Timeslot.create(time: "10 AM", student_id: user2.id, tutor_id: user1.id, topic_id: topic3.id)
-Timeslot.create(time: "11 AM", student_id: user2.id, tutor_id: user1.id, topic_id: topic3.id)
-Timeslot.create(time: "12 PM", student_id: user2.id, tutor_id: user1.id, topic_id: topic3.id)
-Timeslot.create(time: "1 PM", student_id: user3.id, tutor_id: user1.id, topic_id: topic3.id)
-Timeslot.create(time: "2 PM", student_id: user3.id, tutor_id: user1.id, topic_id: topic3.id)
-Timeslot.create(time: "3 PM", student_id: user3.id, tutor_id: user1.id, topic_id: topic3.id)
-Timeslot.create(time: "4 PM", student_id: user4.id, tutor_id: user1.id, topic_id: topic3.id)
-Timeslot.create(time: "5 PM", student_id: user4.id, tutor_id: user1.id, topic_id: topic3.id)
+Timeslot.create(time: "11 AM", student_id: user3.id, tutor_id: user1.id, topic_id: topic3.id)
+Timeslot.create(time: "12 PM", student_id: user4.id, tutor_id: user1.id, topic_id: topic3.id)
+# Timeslot.create(time: "1 PM", student_id: nil, tutor_id: user1.id, topic_id: topic3.id)
+Timeslot.create(time: "2 PM", student_id: nil, tutor_id: user1.id, topic_id: topic3.id)
+Timeslot.create(time: "3 PM", student_id: nil, tutor_id: user1.id, topic_id: topic3.id)
+Timeslot.create(time: "4 PM", student_id: nil, tutor_id: user1.id, topic_id: topic3.id)
+Timeslot.create(time: "5 PM", student_id: nil, tutor_id: user1.id, topic_id: topic3.id)
+
+
+# Timeslot.create(time: "10 AM", student_id: nil, tutor_id: user2.id, topic_id: topic3.id)
+Timeslot.create(time: "11 AM", student_id: user4.id, tutor_id: user2.id, topic_id: topic3.id)
+Timeslot.create(time: "12 PM", student_id: user3.id, tutor_id: user2.id, topic_id: topic3.id)
+Timeslot.create(time: "1 PM", student_id: user1.id, tutor_id: user2.id, topic_id: topic3.id)
+Timeslot.create(time: "2 PM", student_id: nil, tutor_id: user2.id, topic_id: topic3.id)
+Timeslot.create(time: "3 PM", student_id: nil, tutor_id: user2.id, topic_id: topic3.id)
+Timeslot.create(time: "4 PM", student_id: nil, tutor_id: user2.id, topic_id: topic3.id)
+Timeslot.create(time: "5 PM", student_id: nil, tutor_id: user2.id, topic_id: topic3.id)
+
+Timeslot.create(time: "10 AM", student_id: user4.id, tutor_id: user3.id, topic_id: topic3.id)
+# Timeslot.create(time: "11 AM", student_id: nil, tutor_id: user3.id, topic_id: topic3.id)
+# Timeslot.create(time: "12 PM", student_id: nil, tutor_id: user3.id, topic_id: topic3.id)
+Timeslot.create(time: "1 PM", student_id: nil, tutor_id: user3.id, topic_id: topic3.id)
+Timeslot.create(time: "2 PM", student_id: user1.id, tutor_id: user3.id, topic_id: topic3.id)
+Timeslot.create(time: "3 PM", student_id: user2.id, tutor_id: user3.id, topic_id: topic3.id)
+Timeslot.create(time: "4 PM", student_id: nil, tutor_id: user3.id, topic_id: topic3.id)
+Timeslot.create(time: "5 PM", student_id: nil, tutor_id: user3.id, topic_id: topic3.id)
+
+# Timeslot.create(time: "10 AM", student_id: nil, tutor_id: user4.id, topic_id: topic3.id)
+# Timeslot.create(time: "11 AM", student_id: nil, tutor_id: user4.id, topic_id: topic3.id)
+# Timeslot.create(time: "12 PM", student_id: nil, tutor_id: user4.id, topic_id: topic3.id)
+Timeslot.create(time: "1 PM", student_id: nil, tutor_id: user4.id, topic_id: topic3.id)
+Timeslot.create(time: "2 PM", student_id: user1.id, tutor_id: user4.id, topic_id: topic3.id)
+Timeslot.create(time: "3 PM", student_id: user2.id, tutor_id: user4.id, topic_id: topic3.id)
+Timeslot.create(time: "4 PM", student_id: user3.id, tutor_id: user4.id, topic_id: topic3.id)
+Timeslot.create(time: "5 PM", student_id: nil, tutor_id: user4.id, topic_id: topic3.id)
 
 
 
