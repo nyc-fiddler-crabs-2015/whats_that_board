c976e53c339c1c7b55133b9e3aaf85a394d8db6b
gabalicious <gabalicious113@gmail.com>
2015-02-27 21:36:41 -0500

    Fix Seed file

--- db/seeds.rb
+++ db/seeds.rb
@@ -9,14 +9,14 @@ topic2 = Topic.create(category: "Phase 1 / Week 2" , title: "OO Design")
 topic3 = Topic.create(category: "Phase 2 / Week 1" , title: "RESTful Routes")
 
 
-Timeslot.create(time: "10 AM", student: user2, tutor: user1, topic: topic3)
-Timeslot.create(time: "11 AM", student: user2, tutor: user1, topic: topic3)
-Timeslot.create(time: "12 PM", student: user2, tutor: user1, topic: topic3)
-Timeslot.create(time: "1 PM", student: user3, tutor: user1, topic: topic3)
-Timeslot.create(time: "2 PM", student: user3, tutor: user1, topic: topic3)
-Timeslot.create(time: "3 PM", student: user3, tutor: user1, topic: topic3)
-Timeslot.create(time: "4 PM", student: user4, tutor: user1, topic: topic3)
-Timeslot.create(time: "5 PM", student: user4, tutor: user1, topic: topic3)
+Timeslot.create(time: "10 AM", student_id: user2.id, tutor_id: user1.id, topic_id: topic3.id)
+Timeslot.create(time: "11 AM", student_id: user2.id, tutor_id: user1.id, topic_id: topic3.id)
+Timeslot.create(time: "12 PM", student_id: user2.id, tutor_id: user1.id, topic_id: topic3.id)
+Timeslot.create(time: "1 PM", student_id: user3.id, tutor_id: user1.id, topic_id: topic3.id)
+Timeslot.create(time: "2 PM", student_id: user3.id, tutor_id: user1.id, topic_id: topic3.id)
+Timeslot.create(time: "3 PM", student_id: user3.id, tutor_id: user1.id, topic_id: topic3.id)
+Timeslot.create(time: "4 PM", student_id: user4.id, tutor_id: user1.id, topic_id: topic3.id)
+Timeslot.create(time: "5 PM", student_id: user4.id, tutor_id: user1.id, topic_id: topic3.id)
 
 
 
