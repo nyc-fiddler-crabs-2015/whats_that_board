c59435f2291ddd950d4f0591684091834bd378f6
gabalicious <gabalicious113@gmail.com>
2015-03-01 20:53:25 -0500

    Fully functioning dashboard

--- app/controllers/timeslots.rb
+++ app/controllers/timeslots.rb
@@ -12,5 +12,46 @@ post '/timeslots/new' do
 	params[:tutor_id] = session[:user_id]
 	p params
   @timeslot = Timeslot.create(params)
+  redirect back
 end
 
+get '/timeslots/:id/add_student' do
+  @topics = Topic.all
+  @users = User.all
+  @timeslot_id = params[:id]
+  @time = params[:time]
+  erb :'timeslots/add_student'
+end
+
+
+get '/timeslots/be_student' do
+  @topics = Topic.all
+  @users = User.all
+   @time = params[:time]
+  @timeslots = Timeslot.where(time: @time)
+  erb :'timeslots/be_student'
+end
+
+put '/timeslots/be_student' do
+	puts "\n\n\n\n"
+	p params
+	@timeslot = Timeslot.update(params[:id], :topic_id => params[:topic_id], :student_id => current_user.id)
+	redirect "/users/#{current_user.id}"
+end
+put '/timeslots/:id' do
+	puts "\n\n\n\n"
+	p params
+	@timeslot = Timeslot.update(params[:id], :topic_id => params[:topic_id], :student_id => params[:student_id])
+	redirect "/"
+end
+
+delete '/timeslots/:id' do
+	@timeslot = Timeslot.delete(params[:id])
+	redirect back
+end
+
+
+put '/timeslots/:id/remove_student' do
+	@timeslot = Timeslot.update(params[:id], :topic_id => nil, :student_id => nil)
+	redirect "/"
+end
--- app/helpers/test.rb
+++ app/helpers/test.rb
@@ -30,15 +30,83 @@ def boooked_me_student
 	return @timeslots
 end
 
-def check_status(time)
-	@timeslot = Timeslot.find_by(student_id: current_user.id, time: time)
-	@timeslot2 = Timeslot.find_by(student_id: current_user.id, time: time)
-	if (@timeslot.class == Timeslot)
-		return @timeslot
+def check_status(time,user_id)
+	@learning = Timeslot.find_by(student_id: user_id, time: time)
+	@tutor_free = Timeslot.find_by(tutor_id: user_id, time: time, student_id: nil)
+	@tutor_booked = Timeslot.find_by(tutor_id: user_id, time: time)
+	if (@learning.class == Timeslot)
+		return "Learning with #{@learning.tutor.name}"
+	elsif (@tutor_free.class == Timeslot)
+		return  "Tutoring no one yet"
+	elsif(@tutor_booked.class == Timeslot)
+		return "Tutoring #{@tutor_booked.student.name}"
 	else
-		return false
+		return "Free"
 	end
-
-
 	 
+end
+
+def place_buttons(time,user_id)
+	@learning = Timeslot.find_by(student_id: user_id, time: time)
+	@tutor_free = Timeslot.find_by(tutor_id: user_id, time: time, student_id: nil)
+	@tutor_booked = Timeslot.find_by(tutor_id: user_id, time: time)
+	if (@learning.class == Timeslot)
+		return cancel_button_as_tutor(@learning)
+	elsif (@tutor_free.class == Timeslot)
+		return add_student(@tutor_free) + cancel_button_as_tutor(@tutor_free)
+	elsif(@tutor_booked.class == Timeslot)
+		return cancel_button_as_student(@tutor_booked)
+	else
+		return pick_tutor(time) + be_tutor(time)
+	end
+end
+
+def cancel_button_as_tutor(timeslot)
+	return   <<-HTML
+  <form action="/timeslots/#{timeslot.id}" method="post">
+   <input type="hidden" name="_method" value="DELETE">
+  <input type="submit" value="Cancel Session" class="btn"/>
+</form>
+  HTML
+	
+end
+
+def cancel_button_as_student(timeslot)
+	return   <<-HTML
+  <form action="/timeslots/#{timeslot.id}/remove_student" method="post">
+   <input type="hidden" name="_method" value="PUT">
+  <input type="submit" value="Remove Student" class="btn"/>
+</form>
+  HTML
+	
+end
+
+def add_student(timeslot)
+	return   <<-HTML
+  <form action="/timeslots/#{timeslot.id}/add_student" method="get">
+     <input type="hidden" name="time" value="#{timeslot.time}">
+  <input type="submit" value="Add Student" class="btn"/>
+</form>
+  HTML
+	
+end
+
+def pick_tutor(timeslot)
+	return   <<-HTML
+  <form action="/timeslots/be_student" method="get">
+     <input type="hidden" name="time" value="#{timeslot}">
+  <input type="submit" value="Pick Tutor" class="btn"/>
+</form>
+  HTML
+	
+end
+
+def be_tutor(timeslot)
+	return   <<-HTML
+  <form action="/timeslots/new" method="post">
+     <input type="hidden" name="time" value="#{timeslot}">
+  <input type="submit" value="Be Tutor" class="btn"/>
+</form>
+  HTML
+	
 end
